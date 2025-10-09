ecd9cdb29df5a3e980a0166f25f4c28da6165bf0
Merritt M. <40345969+merrittcmason@users.noreply.github.com>
2025-10-09 17:42:10 -0400

    Updated WelcomeScreen.tsx

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './WelcomeScreen.css';
 
 interface WelcomeScreenProps {
@@ -7,14 +7,28 @@ interface WelcomeScreenProps {
 }
 
 export function WelcomeScreen({ userName, onComplete }: WelcomeScreenProps) {
+  const [firstTime, setFirstTime] = useState(false);
+
   useEffect(() => {
-    const timer = setTimeout(onComplete, 2000);
+    const firstLoginKey = `firstLogin_${userName}`;
+    if (!localStorage.getItem(firstLoginKey)) {
+      setFirstTime(true);
+      localStorage.setItem(firstLoginKey, 'true');
+    } else {
+      setFirstTime(false);
+    }
+  }, [userName]);
+
+  useEffect(() => {
+    const timer = setTimeout(onComplete, 2500);
     return () => clearTimeout(timer);
   }, [onComplete]);
 
   return (
     <div className="welcome-screen">
-      <h1 className="welcome-text">Welcome back, {userName}!</h1>
+      <h1 className="welcome-text">
+        {firstTime ? `Welcome ${userName}!` : `Welcome back, ${userName}!`}
+      </h1>
     </div>
   );
 }
